014144a09256f9c6f0725e7fc17284c45c8ea25b
Tommy <t3ma2002@mail.ru>
2020-05-29 16:44:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-bar').addEventListener('click', function () {
+	document.querySelector('.menu-bar span').classList.toggle('active');
+	document.querySelector('.menu').classList.toggle('animate');
+
+})
